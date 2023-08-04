4a6bdf1324a3751b38938bc06bb7e3e0a76d33a8
Rowoon Lee <dev.rowoon@gmail.com>
2023-08-04 21:15:57 +0900

    :art: 재고 개수 관한 메소드 생성

--- src/Kiosk.java
+++ src/Kiosk.java
@@ -1,9 +1,27 @@
 public class Kiosk {
     public static final int KEY = 3;
-    public int inventory;
+    private int inventory;
 
+    // 키오스크 생성자
     public Kiosk(){
         this.inventory = inventory;
     }
+
+    // 재고 개수 확인하여 불린 타입 반환
+    public boolean isInvetory(int count){
+        if(inventory > count){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    // 재고개수를 감소시켜줌
+    public void subInventory(int count){
+        this.inventory = inventory - count;
+    }
+
+
+
 
 }
